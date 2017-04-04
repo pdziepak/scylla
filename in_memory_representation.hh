f19a003b6fe4b897f3866f92e01756e46b3b1a58
Paweł Dziepak <pdziepak@scylladb.com>
2017-04-04 12:36:01 +0100

    imr: generalise method generation for any method

--- in_memory_representation.hh
+++ in_memory_representation.hh
@@ -907,15 +907,21 @@ public:
 
 namespace methods {
 
-struct trivial_destructor {
+template<template<class> typename Method>
+struct trivial_method {
     static void run(...) noexcept { }
 };
 
 template<typename T>
-struct destructor : trivial_destructor { };
+struct destructor : trivial_method<destructor> { };
+
+using trivial_destructor = trivial_method<destructor>;
+
+template<template<class> typename Method, typename T>
+using has_trivial_method = std::is_base_of<trivial_method<Method>, Method<T>>;
 
 template<typename T>
-using is_trivially_destructible = std::is_base_of<trivial_destructor, destructor<T>>;
+using is_trivially_destructible = has_trivial_method<destructor, T>;
 
 namespace internal {
 
@@ -929,76 +935,92 @@ constexpr bool conjunction() noexcept {
 
 }
 
-template<typename T, typename... Members>
-struct generate_destructor : trivial_destructor { };
+template<template<class> typename Method, typename...>
+struct generate_method : trivial_method<Method> { };
 
-template<typename Structure, typename... Tags, typename... Types>
-struct generate_destructor<Structure, member<Tags, Types>...> {
+template<template<class> typename Method, typename Structure, typename... Tags, typename... Types>
+struct generate_method<Method, Structure, member<Tags, Types>...> {
     template<typename Context>
     static void run(const uint8_t* ptr, const Context& context) noexcept {
         auto view = Structure::make_view(ptr);
-        auto ignore_me = { 0, (destructor<Types>::run(ptr + view.template offset_of<Tags>(), context), 0)... };
+        auto ignore_me = { 0, (Method<Types>::run(ptr + view.template offset_of<Tags>(), context), 0)... };
         (void)ignore_me;
     }
 };
 
-template<typename Tag, typename Type>
-struct generate_destructor<optional<Tag, Type>> {
+template<template<class> typename Method, typename Tag, typename Type>
+struct generate_method<Method, optional<Tag, Type>> {
     template<typename Context>
     static void run(const uint8_t* ptr, const Context& context) noexcept {
         if (context.template is_present<Tag>()) {
-            destructor<Type>::run(ptr, context);
+            Method<Type>::run(ptr, context);
         }
     }
 };
 
-template<typename Tag, typename... Members>
-struct generate_destructor<variant<Tag, Members...>> {
+template<template<class> typename Method, typename Tag, typename... Members>
+struct generate_method<Method, variant<Tag, Members...>> {
     template<typename Context>
     static void run(const uint8_t* ptr, const Context& context) noexcept {
         auto view = variant<Tag, Members...>::make_view(ptr, context);
         view.visit_type([&] (auto alternative_type) {
             using type = std::remove_pointer_t<decltype(alternative_type)>;
-            destructor<type>::run(ptr, context);
+            Method<type>::run(ptr, context);
         }, context);
     }
 };
 
-template<typename T>
-struct member_is_trivially_destructible;
+template<template<class> typename Method>
+struct member_has_trivial_method {
+    template<typename T>
+    struct type;
+};
 
+template<template<class> typename Method>
 template<typename Tag, typename Type>
-struct member_is_trivially_destructible<member<Tag, Type>> : is_trivially_destructible<Type> { };
+struct member_has_trivial_method<Method>::type<member<Tag, Type>> : has_trivial_method<Method, Type> { };
 
 template<template <class> typename Predicate, typename... Ts>
 using all_of = std::integral_constant<bool, internal::conjunction<Predicate<Ts>::value...>()>;
 
-template<typename... Members>
-struct destructor<structure<Members...>>
-    : std::conditional_t<all_of<member_is_trivially_destructible, Members...>::value,
-                         trivial_destructor,
-                         generate_destructor<structure<Members...>, Members...>>
+template<template<class> typename Method, typename T>
+struct get_method;
+
+template<template<class> typename Method, typename... Members>
+struct get_method<Method, structure<Members...>>
+    : std::conditional_t<all_of<member_has_trivial_method<Method>::template type, Members...>::value,
+                         trivial_method<Method>,
+                         generate_method<Method, structure<Members...>, Members...>>
 { };
+
+template<template<class> typename Method, typename Tag, typename Type>
+struct get_method<Method, optional<Tag, Type>>
+    : std::conditional_t<has_trivial_method<Method, Type>::value,
+                         trivial_method<Method>,
+                         generate_method<Method, optional<Tag, Type>>>
+{ };
+
+template<template<class> typename Method, typename Tag, typename... Members>
+struct get_method<Method, variant<Tag, Members...>>
+    : std::conditional_t<all_of<member_has_trivial_method<Method>::template type, Members...>::value,
+                         trivial_method<Method>,
+                         generate_method<Method, variant<Tag, Members...>>>
+{ };
+
+template<typename... Members>
+struct destructor<structure<Members...>> : get_method<destructor, structure<Members...>> { };
 
 template<typename Tag, typename Type>
-struct destructor<optional<Tag, Type>>
-    : std::conditional_t<is_trivially_destructible<Type>::value,
-                         trivial_destructor,
-                         generate_destructor<optional<Tag, Type>>>
-{ };
+struct destructor<optional<Tag, Type>> : get_method<destructor, optional<Tag, Type>> { };
 
 template<typename Tag, typename... Members>
-struct destructor<variant<Tag, Members...>>
-    : std::conditional_t<all_of<member_is_trivially_destructible, Members...>::value,
-                         trivial_destructor,
-                         generate_destructor<variant<Tag, Members...>>>
-{ };
-
+struct destructor<variant<Tag, Members...>> : get_method<destructor, variant<Tag, Members...>> { };
 
 template<typename T, typename Context = decltype(no_context)>
 void destroy(const uint8_t* ptr, const Context& context = no_context) {
     destructor<T>::run(ptr, context);
 }
+
 
 }
 
