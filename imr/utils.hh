3958474603e8616adbdabcfd4c0425c424ac015d
Paweł Dziepak <pdziepak@scylladb.com>
2017-04-10 15:50:28 +0100

    imr/utils: add external object allocator

--- imr/utils.hh
+++ imr/utils.hh
@@ -19,20 +19,47 @@
  * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma
+#pragma once
 
 namespace imr {
 namespace utils {
 
 static struct { } no_context;
 
+template<typename Structure, typename Context>
+class lsa_migrate_fn final : public migrate_fn_type {
+    const Context& _context;
+public:
+    explicit lsa_migrate_fn(const Context& context) noexcept : _context(context) { }
+
+    lsa_migrate_fn(lsa_migrate_fn&&) = delete;
+    lsa_migrate_fn(const lsa_migrate_fn&) = delete;
+
+    lsa_migrate_fn& operator=(lsa_migrate_fn&&) = delete;
+    lsa_migrate_fn& operator=(const lsa_migrate_fn&) = delete;
+
+    virtual void migrate(void* src_ptr, void* dst_ptr, size_t size) const noexcept override {
+        std::memcpy(dst_ptr, src_ptr, size);
+        methods::move<Structure>(static_cast<uint8_t*>(dst_ptr), _context);
+    }
+};
+
+template<typename Structure>
+struct default_lsa_migrate_fn {
+    static lsa_migrate_fn<Structure, decltype(no_context)> migrate_fn;
+};
+
+template<typename Structure>
+lsa_migrate_fn<Structure, decltype(no_context)> default_lsa_migrate_fn<Structure>::migrate_fn(no_context);
+
+
 // LSA-aware helper for creating hybrids of C++ and IMR objects. Particularly
 // useful in the intermediate before we are fully converted to IMR so that we
 // can easily have e.g. IMR objects with boost::intrusive containers hooks.
 template<typename Header, typename Structure>
 GCC6_CONCEPT(requires std::is_nothrow_move_constructible<Header>::value
-                   && std::is_nothrow_destructible<Header::value)
-class object_with_header : Header {
+                   && std::is_nothrow_destructible<Header>::value)
+class object_with_header : public Header {
     using Header::Header;
     ~object_with_header() = default;
     object_with_header(object_with_header&&) = default;
@@ -49,12 +76,12 @@ public:
     object_with_header& operator=(object_with_header&&) = delete;
 
     template<typename Context = decltype(no_context)>
-    Structure::view imr_object(const Context& context = no_context) const noexcept {
+    typename Structure::view imr_object(const Context& context = no_context) const noexcept {
         return Structure::make_view(imr_data(), context);
     }
 
     template<typename Context = decltype(no_context)>
-    Structure::mutable_view imr_object(const Context& context = no_context) const noexcept {
+    typename Structure::mutable_view imr_object(const Context& context = no_context) const noexcept {
         return Structure::make_view(imr_data(), context);
     }
 public:
@@ -96,6 +123,92 @@ public:
         methods::destroy<Structure>(object->imr_data(), context);
         current_allocator().destroy(object);
     }
+};
+
+class external_object_allocator {
+    union allocation {
+    private:
+        void* _pointer;
+        std::pair<size_t, allocation_strategy::migrate_fn> _request; // ensure that pair is a trivially destructible
+    public:
+        explicit allocation(size_t n, allocation_strategy::migrate_fn fn) noexcept
+            : _request(std::make_pair(n, fn)) { }
+
+        void allocate() {
+            auto ptr = current_allocator().alloc(_request.second, _request.first, 1);
+            _pointer = ptr;
+        }
+
+        void free() noexcept {
+            current_allocator().free(_pointer);
+        }
+
+        void* pointer() const noexcept { return _pointer; }
+    };
+    std::deque<allocation> _allocations; // clustered list?
+    size_t _position = 0;
+private:
+    void reserve(size_t n, allocation_strategy::migrate_fn migrate) {
+        _allocations.emplace_back(n, migrate);
+    }
+    uint8_t* get_next() {
+        return static_cast<uint8_t*>(_allocations[_position++].pointer());
+    }
+public:
+    // TODO: split phases into two types to enforce correctness
+
+    size_t requested_allocations_count() const { return _allocations.size(); }
+
+    void allocate_all() {
+        auto it = _allocations.begin();
+        try {
+            // TODO: send batch of allocations to the allocation strategy and
+            // let it worry about it
+            while (it != _allocations.end()) {
+                it->allocate();
+                ++it;
+            }
+        } catch (...) {
+            while (it != _allocations.begin()) {
+                --it;
+                it->free();
+            }
+            throw;
+        }
+    }
+
+    class sizer {
+        external_object_allocator& _parent;
+    public:
+        explicit sizer(external_object_allocator& parent) noexcept
+            : _parent(parent) { }
+
+        // FIXME: allow users to specify their own migrate_fn (for context-dependent
+        // movers).
+        template<typename T, typename... Args>
+        uint8_t* serialize(Args&& ... args) {
+            auto size = T::size_when_serialized(std::forward<Args>(args)...);
+            _parent.reserve(size, &default_lsa_migrate_fn<T>::migrate_fn);
+            return nullptr;
+        }
+    };
+
+    class serializer {
+        external_object_allocator& _parent;
+    public:
+        explicit serializer(external_object_allocator& parent) noexcept
+            : _parent(parent) { }
+
+        template<typename T, typename... Args>
+        uint8_t* serialize(Args&& ... args) noexcept {
+            auto ptr = _parent.get_next();
+            T::serialize(ptr, std::forward<Args>(args)...);
+            return ptr;
+        }
+    };
+
+    sizer get_sizer() noexcept { return sizer(*this); }
+    serializer get_serializer() noexcept { return serializer(*this); }
 };
 
 }
