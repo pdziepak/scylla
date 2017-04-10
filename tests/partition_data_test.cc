d58bbd79f2dd2b5369d407567ff75cab9350fdb1
Paweł Dziepak <pdziepak@scylladb.com>
2017-04-10 15:51:20 +0100

    tests/partition_data: update test for external storage

--- tests/partition_data_test.cc
+++ tests/partition_data_test.cc
@@ -29,6 +29,9 @@
 
 #include "partition_data.hh"
 
+// Don't make me link with the rest of Scylla because of a single symbol.
+standard_allocation_strategy standard_allocation_strategy_instance;
+
 // duplicated from imr_test.cc
 static constexpr auto random_test_iteration_count = 20;
 
@@ -59,24 +62,39 @@ bytes random_bytes() {
 //</duplicated>
 
 BOOST_AUTO_TEST_CASE(test_live_cell_creation) {
+    std::uniform_int_distribution<size_t> length_dist(0, data::cell::maximum_internal_storage_length * 2);
+
     for (auto i : boost::irange(0, random_test_iteration_count)) {
         (void)i;
 
-        auto value = random_bytes();
+        imr::utils::external_object_allocator allocator;
+
+        auto value = random_bytes(length_dist(gen));
         auto timestamp = random_int<api::timestamp_type>();
         data::type_info ti;
 
+        // Phase 1: determine sizes of all objects
         auto builder = data::cell::make_live(ti, timestamp, value);
-        auto expected_size = data::cell::size_of(builder);
-        BOOST_CHECK_GT(expected_size, value.size());
+        auto expected_size = data::cell::size_of(builder, allocator);
         BOOST_TEST_MESSAGE("cell size: " << expected_size);
 
-        auto buffer = std::make_unique<uint8_t[]>(expected_size);
-        BOOST_CHECK_EQUAL(data::cell::serialize(buffer.get(), builder), expected_size);
+        BOOST_CHECK_EQUAL(allocator.requested_allocations_count(),
+                          value.size() > data::cell::maximum_internal_storage_length);
+
+        // Phase 2: allocate necessary buffers
+        allocator.allocate_all();
+        // FIXME: the allocator will not free buffers it allocated, imr objects
+        // need to be serialised and then destroyed.
+
+        // Phase 3: serialise objects
+        auto buffer = std::make_unique<uint8_t[]>(expected_size + 7);
+        BOOST_CHECK_EQUAL(data::cell::serialize(buffer.get(), builder, allocator), expected_size);
 
         auto view = data::cell::make_view(ti, buffer.get());
         BOOST_CHECK(view.is_live());
         BOOST_CHECK_EQUAL(view.timestamp(), timestamp);
         BOOST_CHECK(boost::equal(view.value(), value));
+
+        data::cell::destroy(ti, buffer.get());
     }
 }
