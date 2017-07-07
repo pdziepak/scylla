67d89ff243c317bacad5b2431bab89e5774f7333
Paweł Dziepak <pdziepak@scylladb.com>
2017-07-07 14:55:43 +0100

    lsa: provide migrator with the object size
    While the migration function should have enough information to obtain
    the object size itself, the LSA logic needs to compute it as well.
    IMR is going to make calculating object sizes more expensive, so by
    providing the infromation to the migrator we can avoid some needless
    operations.

--- utils/logalloc.cc
+++ utils/logalloc.cc
@@ -1375,7 +1375,7 @@ private:
             auto size = desc->live_size(obj);
             auto dst = alloc_small(desc->migrator(), size, desc->alignment());
             _sanitizer.on_migrate(obj, size, dst);
-            desc->migrator()->migrate(obj, dst);
+            desc->migrator()->migrate(obj, dst, size);
         });
 
         free_segment(seg, desc);
@@ -1665,7 +1665,7 @@ public:
                 auto size = desc.live_size(pos);
                 offset += size;
                 _sanitizer.on_migrate(pos, size, dpos);
-                desc.migrator()->migrate(const_cast<char*>(pos), dpos);
+                desc.migrator()->migrate(const_cast<char*>(pos), dpos, size);
             } else {
                 offset += desc.dead_size();
             }
