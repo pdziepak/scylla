83106ce65ed008d965d4df75248323f3106cbb9a
Paweł Dziepak <pdziepak@scylladb.com>
2017-08-01 12:38:33 +0100

    combine: add some more algorithms

--- combine.hh
+++ combine.hh
@@ -55,4 +55,78 @@ combine(InputIterator1 begin1, InputIterator1 end1,
     return out;
 }
 
+// TODO: generalise to multiple ranges
+template<typename InputRange1, typename InputRange2, typename Emit, typename LessCompare>
+void combine2(InputRange1&& range1, InputRange2&& range2, Emit&& emit, LessCompare&& less)
+{
+    auto begin1 = range1.begin();
+    auto end1 = range1.end();
+    auto begin2 = range2.begin();
+    auto end2 = range2.end();
+    while (begin1 != end1 && begin2 != end2) {
+        auto&& e1 = *begin1;
+        auto&& e2 = *begin2;
+        if (less(e1, e2)) {
+            emit(e1);
+            ++begin1;
+        } else if (less(e2, e1)) {
+            emit(e2);
+            ++begin2;
+        } else {
+            emit(e1, e2);
+            ++begin1;
+            ++begin2;
+        }
+    }
+    std::for_each(begin1, end1, emit);
+    std::for_each(begin2, end2, emit);
+}
 
+static struct none_t { } none;
+
+template<typename InputRange1, typename InputRange2, typename Emit, typename LessCompare>
+void combine3(InputRange1&& range1, InputRange2&& range2, Emit&& emit, LessCompare&& less)
+{
+    auto begin1 = range1.begin();
+    auto end1 = range1.end();
+    auto begin2 = range2.begin();
+    auto end2 = range2.end();
+    while (begin1 != end1 && begin2 != end2) {
+        auto&& e1 = *begin1;
+        auto&& e2 = *begin2;
+        if (less(e1, e2)) {
+            emit(e1, none);
+            ++begin1;
+        } else if (less(e2, e1)) {
+            emit(none, e2);
+            ++begin2;
+        } else {
+            emit(e1, e2);
+            ++begin1;
+            ++begin2;
+        }
+    }
+    std::for_each(begin1, end1, [&emit] (auto&& e) { emit(e, none); });
+    std::for_each(begin2, end2, [&emit] (auto&& e) { emit(none, e); });
+}
+
+template<typename... Functions>
+struct do_make_visitor {
+    void operator()();
+};
+
+template<typename Function, typename... Functions>
+struct do_make_visitor<Function, Functions...> : Function, do_make_visitor<Functions...> {
+    do_make_visitor(Function fn, Functions... fns)
+        : Function(std::move(fn))
+        , do_make_visitor<Functions...>(std::move(fns)...)
+    { }
+
+    using Function::operator();
+    using do_make_visitor<Functions...>::operator();
+};
+
+template<typename... Functions>
+auto make_visitor(Functions&&... fns) {
+    return do_make_visitor<Functions...>(std::forward<Functions>(fns)...);
+}
