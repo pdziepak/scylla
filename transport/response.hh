a22ea69c53ed92ce963ecba2a4ae3949a0da0457
Paweł Dziepak <pdziepak@scylladb.com>
2018-06-07 16:31:48 +0100

    transport: steal response buffers and make send zero-copy
    Each response is sent only once, so we can safely steal its buffers and
    pass them to the output_stream using the zero-copy interface.

--- transport/response.hh
+++ transport/response.hh
@@ -84,7 +84,7 @@ public:
     void write_value(bytes_opt value);
     void write(const cql3::metadata& m, bool skip = false);
     void write(const cql3::prepared_metadata& m, uint8_t version);
-    future<> output(output_stream<char>& out, uint8_t version, cql_compression compression);
+    scattered_message<char> make_message(uint8_t version, cql_compression compression);
 
     cql_binary_opcode opcode() const {
         return _opcode;
